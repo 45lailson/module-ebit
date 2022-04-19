90f23264ab8f423b7e05dd7a5f145b760e0bb4b3
Lailson Sousa <lailson@bizcommerce.com.br>
2022-04-19 01:10:49 -0300

    corrigindo o registration do module

--- registration.php
+++ registration.php
@@ -1,6 +1,7 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Lailson_Ebit',
+    'Lailson_Ebit', __DIR__ . '/src',
     __DIR__
 );
+
